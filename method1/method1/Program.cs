d6545f4e8084d332792e1eeb8fc8437596829069
FarrukhSadikhov <farrukh.sadikhov@outlook.com>
2021-12-27 15:55:24 +0400

    answer-exercises-methods

---
+++ method1/method1/Program.cs
@@ -0,0 +1,173 @@
+﻿using System;
+
+namespace method1
+{
+    class Program
+    {
+        static string PrintMethod()
+        {
+            Console.WriteLine("Выберите метод для обработки массива \n");
+            Console.WriteLine("1. Найти минимальный элемент массива.");
+            Console.WriteLine("2. Найти максимальный элемент массива.");
+            Console.WriteLine("3. Найти индекс минимального элемента массива.");
+            Console.WriteLine("4. Найти индекс максимального элемента массива.");
+            Console.WriteLine("5. Посчитать сумму элементов массива с нечетными индексами.");
+            Console.WriteLine("6. Сделать реверс массива (массив в обратном направлении).");
+            Console.WriteLine("7. Посчитать количество нечетных элементов массива.");
+            Console.WriteLine("8. Поменять местами первую и вторую половину массива, например, для массива 1 2 3 4, результат 3 4 1 2");
+            string number = Console.ReadLine();
+            return number;
+        }
+        static void Check(string number, double[] arr)
+        {
+            switch (number)
+            {
+                case "1":
+                    MinArr(arr);
+                    break;
+                case "2":
+                    MaxArr(arr);
+                    break;
+                case "3":
+                    IndexMinArr(arr);
+                    break;
+                case "4":
+                    IndexMaxArr(arr);
+                    break;
+                case "5":
+                    SumElementNotEven(arr);
+                    break;
+                case "6":
+                    Reverse(arr);
+                    break;
+                case "7":
+                    CountElementNotEven(arr);
+                    break;
+                case "8":
+                    Replacement(arr);
+                    break;
+            }
+        }
+        static double[] Array(uint nums)
+        {
+            double[] arr = new double[nums];
+            for (int i = 0; i < nums; i++)
+            {
+                Console.Write("Введите элементы :");
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            return arr;
+        }
+        static void MinArr(double[] arr)
+        {
+            double min = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine($"Mинимальный элемент массива :{min}");
+        }
+        static void MaxArr(double[] arr)
+        {
+            double max = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+            }
+            Console.WriteLine($"Максимальный элемент массива :{max}");
+        }
+        static void IndexMinArr(double[] arr)
+        {
+            double min = arr[0];
+            int index = 0;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] < min)
+                {
+                    index = i;
+                }
+            }
+            Console.WriteLine($"Индекс минимального элемента массива :{index}");
+        }
+        static void IndexMaxArr(double[] arr)
+        {
+            double indexmax = arr[0];
+            for (int i = 1; i < arr.Length; i++)
+            {
+                if (arr[i] > indexmax)
+                {
+                    indexmax = i;
+                }
+            }
+            Console.WriteLine($"Индекс максимального элемента массива :{indexmax}");
+        }
+        static void SumElementNotEven(double[] arr)
+        {
+            double sum = 0;
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    sum = sum + arr[i];
+                }
+            }
+            Console.WriteLine($"Cумму элементов массива с нечетными индексами :{sum}");
+        }
+        static void Reverse(double[] arr)
+        {
+            Console.WriteLine($"Массив в обратном направлении :");
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        static void CountElementNotEven(double[] arr)
+        {
+            int count = 0;
+            foreach (int x in arr)
+                if (x % 2 == 1)
+                    count++;
+            Console.WriteLine("Количество нечетных элементов: " + count);
+        }
+        static void Replacement(double[] arr)
+        {
+            Console.WriteLine($"Поменять местами первую и вторую половину массива:");
+            int nums;
+            nums = arr.Length / 2;
+            if (arr.Length % 2 != 0)
+            {
+                nums++;
+            }
+            for (int i = nums; i < arr.Length; i++)
+            {
+                Console.Write(arr[i]);
+            }
+            if (arr.Length % 2 != 0)
+            {
+                Console.Write(arr[arr.Length / 2]);
+            }
+            for (int i = 0; i < arr.Length / 2; i++)
+            {
+                Console.Write(arr[i]);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите длинну массива :");
+            uint nums;
+            nums = Convert.ToUInt32(Console.ReadLine());
+            double[] array = Array(nums);
+            string number = PrintMethod();
+            Check(number, array);
+            Console.ReadKey();
+        }
+    }
+}
+    
+
---
+++ method2/method2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace method2
+{
+    class Program
+    {
+        static string CheckName()
+        {           
+            string name;
+            uint countofInvalid;
+            string symbolInvalid = "1234567890/} {)(|@%$&*!?,.`~=-#";
+            do
+            {
+                countofInvalid = 0;
+                name = Console.ReadLine();
+                foreach (char numb1 in name)
+                {
+                    foreach (char numb2 in symbolInvalid)
+                        if (numb1 == numb2)
+                        {
+                            countofInvalid++;
+                        }
+                }
+                if (countofInvalid > 0)
+                {
+                    Console.WriteLine("Имя введенно не корректно,ведите имя повторно");
+                }
+            }
+            while (countofInvalid > 0);
+            return name;
+        }
+        static uint CheckAge()
+        {
+            uint years;
+            UInt32.TryParse(Console.ReadLine(), out years);
+            if (years < 1 || years > 99)
+            {
+                while (years < 1 || years > 99)
+                {
+                    Console.WriteLine("Ведено недопустимое значение, пожалуйста повторите ввод:");
+                    UInt32.TryParse(Console.ReadLine(), out years);
+                }
+            }
+            return years;
+        }
+        static void Solution(string name,string name2,uint years,uint years2)
+        {
+            uint difference;
+            string nameverification;
+            if (years > years2)
+            {               
+                nameverification = Console.ReadLine();
+                if (nameverification == name)
+                {
+                    difference = years - years2;
+                    Console.WriteLine($"Правильно. Человек по имени {name} старше на {difference} год.");
+                }
+                else if (nameverification != name && nameverification != name2)
+                {
+                    Console.WriteLine("Ошибка! Такого имени нет в базе.");
+                }
+                else if (nameverification != name)
+                {
+                    difference = years - years2;
+                    Console.WriteLine($"Неправильно. Человек по имени {name} старше на {difference} год.");
+                }
+            }
+            else if (years2 > years)
+            {
+                nameverification = Console.ReadLine();
+                if (nameverification != name2 && nameverification != name)
+                {
+                    while (nameverification != name2 && nameverification != name)
+                    {
+                        Console.WriteLine("Ошибка! Такого имени нет в базе. Попробуйте ввести еще раз:");
+                        nameverification = Console.ReadLine();
+                    }
+                }
+                if (nameverification == name2)
+                {
+                    difference = years2 - years;
+                    Console.WriteLine($"Правильно. Человек по имени {name2} старше на {difference} год .");
+                }
+                else if (nameverification != name2 && nameverification != name)
+                {
+                    Console.WriteLine("Ошибка! Такого имени нет в базе.");
+                }
+                else if (nameverification != name2)
+                {
+                    difference = years2 - years;
+                    Console.WriteLine($"Неправильно. Человек по имени {name2} старше на {difference} год.");
+                }
+            }
+            else if (years == years2)
+            {
+                Console.WriteLine($"{name} и {name2} ровесники!");
+            }           
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите имя первого человека:");
+            string name = CheckName();
+            Console.WriteLine("Введите возраст первого человека");
+            uint years = CheckAge();
+            Console.WriteLine("Введите имя второго человека:");
+            string name2 = CheckName();
+            Console.WriteLine("Введите возраст второго человека");
+            uint years2 = CheckAge();
+            Console.WriteLine("Введите имя человека, который по вашему мнению старший:");
+            Solution(name,name2,years,years2);
+            Console.ReadKey();
+        }
+    }
+}
